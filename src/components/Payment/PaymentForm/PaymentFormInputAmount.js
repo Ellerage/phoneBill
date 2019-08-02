8c748ad83b6edc47f83142a9b33671ee1bd9050c
DwOfL <hsmail1742@mail.ru>
2019-08-02 18:00:09 +0400

    Refactoring the mask of number

--- src/components/Payment/PaymentForm/PaymentFormInputAmount.js
+++ src/components/Payment/PaymentForm/PaymentFormInputAmount.js
@@ -1,33 +1,35 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { FromGroup, Label, Input, Small } from './PaymentFormInput.styles'
 
-class PaymentFormInputAmount extends Component {
-  inputAmount = (e) => {
+function PaymentFormInputAmount ({ amount, changeValue }) {
+
+  const inputAmount = (e) => {
     let value = e.target.value;
 
     if ((Number(value) >= 1 || value.length === 0) && Number(value) <= 1000) {
-      this.props.changeValue({name: e.target.name, value})
+      changeValue({
+        name: e.target.name, 
+        value
+        })
     }
   }
 
-  render() {
-    return (
-      <FromGroup>
-        <Label htmlFor="amount">Сумма пополнения ₽</Label>
-        <Input type="text" 
-                id="amount"
-                name="amount"
-                placeholder="Сумма пополнения" 
-                required
-                onChange={this.inputAmount}
-                value={this.props.amount}/>
+  return (
+    <FromGroup>
+      <Label htmlFor="amount">Сумма пополнения ₽</Label>
+      <Input type="text" 
+              id="amount"
+              name="amount"
+              placeholder="Сумма пополнения" 
+              required
+              onChange={inputAmount}
+              value={amount}/>
 
-        <Small id="amountHelp">
-          Минимальная сумма пополнения 1₽. Максимальная 1000₽
-        </Small>
-      </FromGroup>
-    );
-  }
+      <Small id="amountHelp">
+        Минимальная сумма пополнения 1₽. Максимальная 1000₽
+      </Small>
+    </FromGroup>
+  );
 }
 
 export default PaymentFormInputAmount;
--- src/components/Payment/PaymentForm/PaymentFormInputPhone.js
+++ src/components/Payment/PaymentForm/PaymentFormInputPhone.js
@@ -1,34 +1,45 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { FromGroup, Label, Input, Small } from './PaymentFormInput.styles'
 
-class PaymentFormInputPhone extends Component {
-  inputPhone = (e) => {
+function PaymentFormInputPhone ({ phoneNumber, changeValue }) {
+  const inputPhone = (e) => {
     let value = e.target.value;
 
     let x = value.replace(/\D/g, '').match(/(\d{0,1})(\d{0,3})(\d{0,3})(\d{0,2})(\d{0,2})/);
-    let result = !x[2] ? `+${x[1]}` : `+${x[1]} (${x[2]}) ` + x[3] + (x[4] ? `-${x[4]}` : '') + (x[5] ? `-${x[5]}` : '') + (x[6] ? `-${x[6]}` : '');
 
-    this.props.changeValue({name: e.target.name, value: result})
+    /*
+    let result = x[2] ? `+${x[1]} (${x[2]}) ${x[3]}` : `+${x[1]}` +
+                (x[4] ? `-${x[4]}`                   : '') + 
+                (x[5] ? `-${x[5]}`                   : '') + 
+                (x[6] ? `-${x[6]}`                   : '');
+    
+    */
+
+    let result = x[2] ? `+${x[1]} (${x[2]}) ${x[3]}` : `+${x[1]}`;
+
+    for (let i = 4; i < 7; i++) {
+      result += x[i] ? `-${x[i]}` : ''
+    }
+
+    changeValue({name: e.target.name, value: result})
   }
 
-  render() {
-    return (
-      <FromGroup>
-        <Label htmlFor="phoneNumber">Номер телефона</Label>
-        <Input type="tel" 
-                id="phoneNumber"
-                name="phoneNumber"
-                placeholder="Введите номер телефона" 
-                required 
-                onChange={this.inputPhone}
-                value={this.props.phoneNumber}/>
+  return (
+    <FromGroup>
+      <Label htmlFor="phoneNumber">Номер телефона</Label>
+      <Input type="tel" 
+              id="phoneNumber"
+              name="phoneNumber"
+              placeholder="Введите номер телефона" 
+              required 
+              onChange={inputPhone}
+              value={phoneNumber}/>
 
-        <Small id="phoneHelp">
-          Формат номера: +7 (XXX) XXX-XX-XX
-        </Small>
-      </FromGroup>
-    );
-  }
+      <Small id="phoneHelp">
+        Формат номера: +7 (XXX) XXX-XX-XX
+      </Small>
+    </FromGroup>
+  );
 }
 
 export default PaymentFormInputPhone;
