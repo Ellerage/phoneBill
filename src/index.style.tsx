814f1c707eb49a5cbca6b4f5003186186ab5b07e
Eller <hsmail1742@mail.ru>
2019-08-10 12:13:07 +0400

    Add font 'Didact Gothic'

---
+++ src/index.style.tsx
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from 'styled-components';
+import './assets/DidactGothic-Regular.ttf'
+
+export const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    font-family: 'Didact Gothic', sans-serif;
+  }
+`
